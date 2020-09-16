d023ea485360d2fdc31c5ebf994a06567a2ea4e4
faker111 <32220139+faker111@users.noreply.github.com>
2020-09-16 17:19:15 +0800

    构建项目，配置全局样式，路由，elementui

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+//import Login from "../views/Login/index.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    //重定向
+    redirect: "login"
+    //name: "Home",
+    //component: Home
+  },
+  , {
+    path: "/login",
+    name: "Login",
+    //component: Login
+    component: () =>
+      import("../views/Login/index.vue")
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
